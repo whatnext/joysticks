3d68fca2d0befd10cd08970dd8cd156f31054a35
Tom Gracey <tom@virtual.blue>
2025-05-16 17:41:54 +0100

    break out of "populate" loop after timeout to prevent stall

--- joysticks_linux.go
+++ joysticks_linux.go
@@ -32,7 +32,7 @@ func DeviceExists(index uint8) bool {
 // Connect sets up a go routine that puts a joysticks events onto registered channels.
 // to register channels use the returned HID object's On<xxx>(index) methods.
 // Note: only one event, of each type '<xxx>', for each 'index', so re-registering, or deleting, an event stops events going on the old channel.
-// It Needs the HID objects ParcelOutEvents() method to be running to perform routing.(so usually in a go routine.) 
+// It Needs the HID objects ParcelOutEvents() method to be running to perform routing.(so usually in a go routine.)
 func Connect(index int) (d *HID) {
 	r, e := os.OpenFile(string(strconv.AppendUint(inputPathSlice, uint64(index-1), 10)), os.O_RDWR, 0)
 	if e != nil {
@@ -42,28 +42,36 @@ func Connect(index int) (d *HID) {
 	// start thread to read joystick events to the joystick.state osEvent channel
 	go eventPipe(r, d.OSEvents)
 	d.populate()
+
 	return d
 }
 
 // fill in the joysticks available events from the synthetic events burst produced initially by the driver.
 func (d HID) populate() {
-	for buttonNumber, hatNumber, axisNumber := 1, 1, 1; ; {
-		evt := <-d.OSEvents
-		switch evt.Type {
-		case 0x81:
-			d.Buttons[evt.Index] = button{uint8(buttonNumber), toDuration(evt.Time), evt.Value != 0}
-			buttonNumber += 1
-		case 0x82:
-			d.HatAxes[evt.Index] = hatAxis{uint8(hatNumber), uint8(axisNumber), false, toDuration(evt.Time), float32(evt.Value) / maxValue}
-			axisNumber += 1
-			if axisNumber > 2 {
-				axisNumber = 1
-				hatNumber += 1
-			}
-		default:
-			go func() { d.OSEvents <- evt }() // have to consume a real event to know we reached the end of the synthetic burst, so refire it.
-			return
-		}
+	populateLoop: for buttonNumber, hatNumber, axisNumber := 1, 1, 1; ; {
+        select {
+        case evt := <-d.OSEvents:
+    		switch evt.Type {
+    		case 0x81:
+    			d.Buttons[evt.Index] = button{uint8(buttonNumber), toDuration(evt.Time), evt.Value != 0}
+    			buttonNumber += 1
+    		case 0x82:
+    			d.HatAxes[evt.Index] = hatAxis{uint8(hatNumber), uint8(axisNumber), false, toDuration(evt.Time), float32(evt.Value) / maxValue}
+    			axisNumber += 1
+    			if axisNumber > 2 {
+    				axisNumber = 1
+    				hatNumber += 1
+    			}
+    		default:
+    			go func() { d.OSEvents <- evt }() // have to consume a real event to know we reached the end of the synthetic burst, so refire it.
+    			return
+    		}
+
+        case <-time.After(1 * time.Second):
+
+            break populateLoop
+
+        }
 	}
 	return
 }
